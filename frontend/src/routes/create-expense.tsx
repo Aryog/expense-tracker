c8bc092eccd26cb82b5fad56285b51f56274c56c
Aryog <yogesh000aryal@gmail.com>
2024-12-23 21:40:25 +0545

    feat(client): added the expense and create-expense

---
+++ frontend/src/routes/create-expense.tsx
@@ -0,0 +1,115 @@
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { Input } from '@/components/ui/input';
+import { Label } from '@radix-ui/react-label';
+import { Button } from '@/components/ui/button';
+import { useForm } from '@tanstack/react-form';
+
+export const Route = createFileRoute("/create-expense")({
+  component: CreateExpense,
+})
+
+function CreateExpense() {
+  const navigate = useNavigate()
+
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      await new Promise(r => setTimeout(r, 3000))
+      console.log(value)
+      navigate({ to: "/expenses" })
+    },
+  })
+  return (
+    <div className="p-2">
+      <h2>Create Expense</h2>
+      <form.Provider>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            void form.handleSubmit();
+          }}
+          className="flex flex-col gap-y-4 max-w-xl m-auto"
+        >
+          <form.Field
+            name="title"
+            validators={{
+            }}
+            children={(field) => (
+              <div>
+                <Label htmlFor={field.name}>Title</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </div>
+            )}
+          />
+
+          <form.Field
+            name="amount"
+            validators={{
+            }}
+            children={(field) => (
+              <div>
+                <Label htmlFor={field.name}>Amount</Label>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  type="number"
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </div>
+            )}
+          />
+
+          {/* <form.Field
+            name="date"
+            validators={{
+              onChange: createExpenseSchema.shape.date,
+            }}
+            children={(field) => (
+              <div className="self-center">
+                <Calendar
+                  mode="single"
+                  selected={new Date(field.state.value)}
+                  onSelect={(date) =>
+                    field.handleChange((date ?? new Date()).toISOString())
+                  }
+                  className="rounded-md border"
+                />
+                {field.state.meta.touchedErrors ? (
+                  <em>{field.state.meta.touchedErrors}</em>
+                ) : null}
+              </div>
+            )}
+          /> */}
+
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+            children={([canSubmit, isSubmitting]) => (
+              <Button className="mt-4" type="submit" disabled={!canSubmit}>
+                {isSubmitting ? "..." : "Submit"}
+              </Button>
+            )}
+          />
+        </form>
+      </form.Provider>
+    </div>
+  );
+}
